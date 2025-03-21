ab873345544324f5c29e046c4d14d3c979573517
Lucia Hernandez <2az5f6t4k@gmail.com>
2025-03-21 08:52:39 -0600

    Actualizacion de aplicacion

---
+++ Aplicación Web/script.js
@@ -0,0 +1,32 @@
+const { application, json } = require("express");
+function sumar() {
+    const numA = parseFloat(document.getElementById('numero1').value);
+    const numB = parseFloat(document.getElementById('numero2').value);
+
+    //verificar si los valores son validos
+    if (isNaN(numA) || isNaN(numB)) {
+
+        document.getElementById('resultado').textContent = 'Por favor, ingrese números válidos';
+        return;
+    }
+    //Llamar a la Api
+    fetch('', { ///reemplaza con lla Url de tu API si es diferente
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ num1: numA, num2: numB })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                document.getElementById('resultado').textContent = 'Error: ' + data.error;
+            } else {
+                document.getElementById('resultado').textContent = 'Resultado: ' + data.resultado;
+            }
+
+        })
+        .catch(error => {
+            document.getElementById('resultado').textContent = 'Error de conexión: ' + error;
+        });
+}
